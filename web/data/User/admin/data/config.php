1ada77b8e0758687513a5926ec92ef5f1885a618
ptflp <filippov.pa@s-vfu.ru>
2018-02-08 20:53:14 +0900

    upload preinstalled kodexplorer

---
+++ web/data/User/admin/data/config.php
@@ -0,0 +1,13 @@
+<?php exit;?>{
+    "listType": "icon",
+    "listSortField": "name",
+    "listSortOrder": "up",
+    "fileIconSize": "80",
+    "animateOpen": "1",
+    "soundOpen": "0",
+    "theme": "win10",
+    "wall": "http:\/\/www.wallpapermaiden.com\/wallpaper\/5958\/download\/2560x1440\/anime-girl-smiling-elf-ears-shrine-butterfly-sitting-lantern.jpg",
+    "fileRepeat": "replace",
+    "recycleOpen": "1",
+    "resizeConfig": "{\"filename\":250,\"filetype\":80,\"filesize\":80,\"filetime\":215,\"editorTreeWidth\":200,\"explorerTreeWidth\":200}"
+}
